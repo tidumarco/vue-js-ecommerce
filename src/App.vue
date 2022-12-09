d64151f4e09bc566b8e6a592d3276b55d14239bf
Marco Tidu <tidumarco@gmail.com>
2022-12-09 17:33:46 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg'
+const product = ref('Socks')
+
+</script>
+
+<template>
+	<div class="nav-bar"></div>
+	<div class="product-display">
+		<div class="product-container">
+			<div class="product-image">
+				<img v-bind:src="socksGreenImage">
+			</div>
+			<div class="product-info">
+				<h1>{{ product }}</h1>
+			</div>
+		</div>
+	</div>
+</template>
